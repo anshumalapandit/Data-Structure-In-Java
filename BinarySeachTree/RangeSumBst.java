7d7f9cdebcf1a2188ae5c95172f57c26675d8751
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2024-10-25 22:32:09 +0530

    Create RangeSumBst.java

---
+++ BinarySeachTree/RangeSumBst.java
@@ -0,0 +1,36 @@
+// sum of all nodes that lies in the interval of a and b [a,b] closed 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null){
+            return 0;
+        }
+        int sum=0;
+        if(root.val>=low && root.val<=high){
+            sum+=root.val;
+        }
+        if(root.val>low){
+            // go to left
+            sum+=rangeSumBST(root.left,low,high);
+        }
+        if(root.val<high){
+            sum+=rangeSumBST(root.right,low,high);
+        }
+        return sum;
+        
+    }
+}
