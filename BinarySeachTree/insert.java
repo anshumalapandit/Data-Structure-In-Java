b5bceb53d740079de3c39887d8b0d69afb88718a
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2024-11-09 00:50:19 +0530

    Update insert.java

--- BinarySeachTree/insert.java
+++ BinarySeachTree/insert.java
@@ -4,10 +4,11 @@ public static Node insert(Node root,int val) {
 		return root;
 	}
 	if(root.data>val) {
+		// if value is less go to left subtree
 		root.left=insert(root.left,val);
 	}else {
-		//if value greater than root go to right
+		//if value greater than root go to right subtree
 		root.right=insert(root.right,val);
 	}
-	return root; //root hi return krna hai..
+	return root; //return the updated root after insertion
 }
