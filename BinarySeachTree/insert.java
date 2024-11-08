6b9a53c64e9f64e844e77c25fcad5a0130dfc6fb
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2024-11-09 00:48:45 +0530

    Update insert.java

--- BinarySeachTree/insert.java
+++ BinarySeachTree/insert.java
@@ -1,1 +1,13 @@
-
+public static Node insert(Node root,int val) {
+	if(root==null) {
+		root=new Node(val);
+		return root;
+	}
+	if(root.data>val) {
+		root.left=insert(root.left,val);
+	}else {
+		//if value greater than root go to right
+		root.right=insert(root.right,val);
+	}
+	return root; //root hi return krna hai..
+}
