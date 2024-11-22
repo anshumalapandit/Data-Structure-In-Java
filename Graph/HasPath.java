bf61a82ae8e39d1e0db27b965f156526efcf9979
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2024-11-23 00:43:25 +0530

    Update HasPath.java

--- Graph/HasPath.java
+++ Graph/HasPath.java
@@ -1,1 +1,59 @@
+package graph;
+import java.util.*;
+
+public class HasPath {
+public static class Edge{
+	int src;
+	int dest;
+	Edge(int s,int d){
+		this.src=s;
+		this.dest=d;
+	}
+}
+public static void createGraph(ArrayList<Edge>[]graph) {
+	for(int i=0;i<graph.length;i++) {
+		graph[i]=new ArrayList<>();
+	}
+	graph[0].add(new Edge(0,1));
+	graph[0].add(new Edge(0,2));
+	graph[1].add(new Edge(1,0));
+	graph[1].add(new Edge(1,3));
+	graph[2].add(new Edge(2,0));
+	graph[2].add(new Edge(2,4));
+	graph[3].add(new Edge(3,1));
+	graph[3].add(new Edge(3,4));
+	graph[3].add(new Edge(3,5));
+	graph[4].add(new Edge(4,2));
+	graph[4].add(new Edge(4,3));
+	graph[4].add(new Edge(4,5));
+	graph[5].add(new Edge(5,3));
+	graph[5].add(new Edge(5,4));
+	graph[5].add(new Edge(5,6));
+	graph[6].add(new Edge(6,5));
+}
+public static boolean haspath(ArrayList<Edge>graph[],int src,int dest,boolean vis[]) {
+	//we will use dfs approach 
+	if(src==dest) {
+		return true;
+	}
+	vis[src]=true;
+	for(int i=0;i<graph[src].size();i++) {
+		Edge e=graph[src].get(i);
+		if(!vis[e.dest] && haspath(graph,e.dest,dest,vis)) { //recursive call
+			//now src=neighbour ban jayega
+			return true;
+		}
+	}
+	return false;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int V=7;
+		ArrayList<Edge>[]graph=new ArrayList[V];
+		createGraph(graph);
+		System.out.println(haspath(graph,0,5,new boolean[V]));
+
+	}
+
+}
 
