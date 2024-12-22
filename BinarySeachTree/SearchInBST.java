b7cc507d1bd4d3ef257803da20e026924cdc217a
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2024-12-22 18:53:36 +0530

    Update SearchInBST.java

--- BinarySeachTree/SearchInBST.java
+++ BinarySeachTree/SearchInBST.java
@@ -16,3 +16,39 @@ public static Node searchBST(Node root, int val) {
       }
 
   }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// if u told to return subtree of that root:
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null){
+            return null;
+        }
+        if(val==root.val){
+            // return subtree rooted with that node
+            return root;
+            
+        }
+        if(val<root.val){
+            // go to left
+            return searchBST(root.left,val);
+        }else if(val>root.val){
+            return searchBST(root.right,val);
+        }
+        return root;
+    }
+}
