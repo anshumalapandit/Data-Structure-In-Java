6da2cb8ca4ae2ef77a56a3b4c4cdc94ed361d1af
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-01-26 10:19:23 +0530

    Create 693. Binary Number with alternating bits.java

---
+++ Bits Manipulation/693. Binary Number with alternating bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int prevBit=(n&1);
+        while(n!=0){
+            n>>=1;
+            if(prevBit==(n&1))
+            return false;
+            prevBit=(n&1);
+        }
+        return true;
+    }
+}
