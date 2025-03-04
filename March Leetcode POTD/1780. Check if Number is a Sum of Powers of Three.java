41e2e25b3c0099e7534d2d74f5b5929ffa5f2c09
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-03-04 07:31:21 +0530

    Update 1780. Check if Number is a Sum of Powers of Three.java

--- March Leetcode POTD/1780. Check if Number is a Sum of Powers of Three.java
+++ March Leetcode POTD/1780. Check if Number is a Sum of Powers of Three.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
         // divide repeatedly 3 
+        // we are dividing by 3 so possible remainder w.k.t [0,2]
         while(n>0){
             if(n%3==2){
                 return false; 
