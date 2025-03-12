b615c19828820b7fa6f6ab795ea4e2f2d49ed62b
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-03-12 16:19:27 +0530

    Update 326. Power of Three.java

--- 1D-2d Matrix(basics)/326. Power of Three.java
+++ 1D-2d Matrix(basics)/326. Power of Three.java
@@ -26,3 +26,15 @@ class Solution {
         return false;
     }
 }
+// optimised way :
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<1){
+            return false;
+        }
+         while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
