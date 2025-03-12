c5f45cc9808131de62a6488cf2a6868218bbbbac
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-03-12 16:17:46 +0530

    Create 326. Power of Three.java

---
+++ 1D-2d Matrix(basics)/326. Power of Three.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<1){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        long left=1;
+        long right=n;
+        while(left<=right){
+            long mid=(left+right)/2;
+            long power=(long) Math.pow(3, mid); // remember power of 3 chahiye not cube
+            if(power==n){
+                return true;
+            }else if(power<n){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+            // Avoid overflow
+            // if (power> n || power < 0) {
+            //     right = mid - 1;
+            // }
+        }
+        return false;
+    }
+}
