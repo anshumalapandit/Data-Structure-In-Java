a3994fc88a121ae45489ff619fd5e1b5422ea6d0
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-06-28 00:41:24 +0530

    Update 2265. Count Nodes Equal to Average of Subtree.java

--- BinaryTree/2265. Count Nodes Equal to Average of Subtree.java
+++ BinaryTree/2265. Count Nodes Equal to Average of Subtree.java
@@ -54,3 +54,55 @@ class Solution {
         return count;
     }
 }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int averageOfSubtree(TreeNode root) {
+        int arr[] = new int[1]; //count
+        dfs(root , arr);
+        return arr[0];
+    }
+
+    public static List<Integer> dfs(TreeNode node , int arr[]){
+        if (node == null){
+            List <Integer> base = new ArrayList<>();
+            return base;
+        }
+        List <Integer> left = dfs(node.left , arr); //8 , 0 , 1
+        List <Integer> right = dfs(node.right , arr); //5 ,6
+        List <Integer> list = new ArrayList<>(); //8 , 0 , 1 , 5 , 6 , 4
+        addElements(left , list);
+        addElements(right , list);
+        list.add(node.val);
+        int average = (int) sum(list)/list.size();
+        if (average == node.val){
+            arr[0] = arr[0] + 1;
+        }
+        return list;
+    }
+    public static void addElements(List <Integer> l , List <Integer> list){
+        for (int i = 0 ; i < l.size() ; i++){
+            list.add(l.get(i));
+        }
+    }
+    public static int sum(List <Integer> list){
+        int sum = 0;
+        for (int i = 0 ; i < list.size() ; i++){
+            sum += list.get(i);
+        }
+        return sum;
+    }
+}
