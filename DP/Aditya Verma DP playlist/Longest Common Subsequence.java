4a5508790544e5bdf205098cfd37676496044557
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-09-16 13:59:32 +0530

    Update Longest Common Subsequence.java

--- DP/Aditya Verma DP playlist/Longest Common Subsequence.java
+++ DP/Aditya Verma DP playlist/Longest Common Subsequence.java
@@ -50,3 +50,38 @@ class Solution {
         return helper(s1,s2,n,m);
     }
 }
+// Lets convert this code in tabulation:
+class Solution {
+    static int dp[][];
+    public static int helper(String s1,String s2,int n,int m){
+       // initialization
+       for(int i=0;i<n+1;i++){
+           for(int j=0;j<m+1;j++){
+               if(i==0 || j==0){
+                   dp[i][j]=0;
+               }
+           }
+       }
+    
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+              if(s1.charAt(i-1)==s2.charAt(j-1)){
+            dp[i][j]=1+dp[i-1][j-1];
+        }else{
+            dp[i][j]=Math.max(dp[i-1][j] ,dp[i][j-1]);
+        }
+            }
+        }
+        return dp[n][m];
+    }
+    static int lcs(String s1, String s2) {
+        // code here
+        // Recursive approach 
+        int n=s1.length();
+        int m=s2.length();
+        dp=new int[n+1][m+1];
+        // now initialize kro with -1 taki answer store hua ki nhi pata laga paye
+         for (int[] row : dp) Arrays.fill(row, -1);
+        return helper(s1,s2,n,m);
+    }
+}
