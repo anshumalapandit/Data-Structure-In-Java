1a55351a144e5677f7c7010a4077b4201f98198f
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-10-04 00:58:34 +0530

    Update 746. Min Cost Climbing Stairs.java

--- DP/LeetCode Easy/746. Min Cost Climbing Stairs.java
+++ DP/LeetCode Easy/746. Min Cost Climbing Stairs.java
@@ -40,3 +40,24 @@ class Solution {
         return ans;
     }
 }
+// tabulation  bottom up
+import java.util.Arrays;
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int dp[] = new int[n+1];  // dp[i] = min cost to reach step i
+
+        // base cases
+        dp[0] = 0;  // cost to "stand" before step 0
+        dp[1] = 0;  // cost to "stand" before step 1
+
+        // build bottom-up
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i-1] + cost[i-1], 
+                             dp[i-2] + cost[i-2]);
+        }
+
+        return dp[n]; // min cost to reach the "top"
+    }
+}
