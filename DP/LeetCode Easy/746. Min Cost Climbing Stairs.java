1b5ee8ae102834d928fe61b124e0b69e47c0833c
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-10-04 00:44:27 +0530

    Update 746. Min Cost Climbing Stairs.java

--- DP/LeetCode Easy/746. Min Cost Climbing Stairs.java
+++ DP/LeetCode Easy/746. Min Cost Climbing Stairs.java
@@ -15,3 +15,28 @@ class Solution {
         return ans;
     }
 }
+// Memoization
+class Solution {
+    static int dp[];
+    public int solve(int idx,int cost[]){
+        // base case
+        if(idx>=cost.length){
+            return 0;
+        }
+        // dp=new int[cost.length+1]; // imp either initialise in main fxn dont here it will reset dp[] in each recursive call
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int curCost=cost[idx]+solve(idx+1,cost);
+        int curCost2=cost[idx]+solve(idx+2,cost);
+        return dp[idx]=Math.min(curCost,curCost2);
+    }
+    public int minCostClimbingStairs(int[] cost) {
+        dp=new int[cost.length+1];
+        Arrays.fill(dp,-1);
+        int a=solve(0,cost);
+        int b=solve(1,cost);
+        int ans=Math.min(a,b);
+        return ans;
+    }
+}
