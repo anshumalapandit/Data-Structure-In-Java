4c71f58297019778d4de5f551cb9879dc5796fe7
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2025-12-05 21:12:40 +0530

    Update 2. Add Two Numbers.java

--- LinkedList/2. Add Two Numbers.java
+++ LinkedList/2. Add Two Numbers.java
@@ -9,46 +9,26 @@
  * }
  */
 class Solution {
-   
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode h1=l1;
-        ListNode h2=l2;
+        ListNode p1=l1;
+        ListNode p2=l2;
         ListNode dummy=new ListNode(-1);
-        ListNode p1=h1;
-        ListNode p2=h2;
         ListNode d=dummy;
         int carry=0;
-        while(p1!=null && p2!=null){
-            int sum=p1.val+p2.val;
-            sum+=carry;
-            int val=sum%10; // last digit
+        while(p1!=null || p2!=null){
+            int v1=p1!=null ? p1.val: 0;
+            int v2=p2!=null ? p2.val:0;
+            int sum=carry+v1+v2;
             carry=sum/10;
-            d.next=new ListNode(val);
+            d.next=new ListNode(sum%10);
             d=d.next;
-            p1=p1.next;
-            p2=p2.next;
+        p1=p1==null? null : p1.next;
+        p2=p2==null? null: p2.next;
         }
-        while(p1!=null && p2==null){
-            int sum=p1.val;
-            sum+=carry;
-            int val=sum%10;
-            carry=sum/10;
-            d.next=new ListNode(val);
-            d=d.next;
-            p1=p1.next;
-        }
-        while(p1==null && p2!=null){
-            int sum=p2.val;
-            sum+=carry;
-            int val=sum%10;
-            carry=sum/10;
-            d.next=new ListNode(val);
-            d=d.next;
-            p2=p2.next;
-        }
-        if(carry!=0){
-            d.next=new ListNode(carry);
-        }
-        return dummy.next;
+       if(carry==1){
+        d.next=new ListNode(carry);
+        d=d.next;
+       }
+       return dummy.next;
     }
 }
