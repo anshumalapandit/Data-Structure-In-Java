565d020f78dfa61567d7d60a179e75cb69b166b6
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2026-03-03 15:47:16 +0530

    Update Longest subarray with Atmost two distinct integers.java

--- Sliding Window/Longest subarray with Atmost two distinct integers.java
+++ Sliding Window/Longest subarray with Atmost two distinct integers.java
@@ -36,3 +36,46 @@ class Solution {
         return ans;
     }
 }
+// optimal: class Solution {
+    public int totalElements(int[] arr) {
+        // code here
+        int n=arr.length;
+        int i=0;
+        int j=0;
+        int ans=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        while(j<n){
+            map.put(arr[j],map.getOrDefault(arr[j],0)+1);
+            // if freq >k 
+            if(map.size()>2){
+                // remove 
+                while(map.size()>2){
+                    int freq=map.get(arr[i]);
+                    if(freq-1==0){
+                        map.remove(arr[i]);
+                    }else{
+                        map.put(arr[i],freq-1);
+                    }
+                    i++;
+                }
+            }
+            //Longest subarray with at most 2 distinct elements not exactly 2 why u r checking==2?
+            if(map.size()<=2){ // imp <=
+                // count all freqs 
+                // int count=0;
+                // for(int key:map.keySet()){
+                //     count+=map.get(key);
+                // }
+                // if(count>ans){
+                //     ans=count;
+                // }
+                int curCount=j-i+1;  // these will give u total elements count in current window
+                if(curCount>ans){
+                    ans=curCount;
+                }
+            }
+            j++;
+        }
+        return ans;
+    }
+}
